98be622bd2c2c9fe5a336e64a1d45a78df445cf6
PullPushSingar <259550@student.pwr.edu.pl>
2024-03-17 21:04:01 +0100

    finish day 1 project and finish day one

---
+++ Day_1/Project_Band_Name_Generator/main.py
@@ -0,0 +1,13 @@
+
+
+
+print("Welcome to the Band Name Generator.")
+origin_city = input("What's the name of the city you grew up in\n")
+pet_name = input("What's your pet\'s name\n")
+
+result_band_name = origin_city +"\'s  " + pet_name + "s"
+
+print("your band name should be : "+ result_band_name)
+
+
+
